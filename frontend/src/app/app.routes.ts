176c51041b9c27d9d8b1b2c407dd7f194cb31049
RAgredaIpar <r.agredaipar@gmail.com>
2025-07-25 19:11:06 -0500

    style(auth): rediseño visual completo de login y registro con layout moderno y colores base

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/posts/pages/home/home.component';
 import { LoginComponent } from './modules/auth/pages/login/login.component';
 import { RegisterComponent } from './modules/auth/pages/register/register.component';
+import { AuthLayoutComponent } from './modules/auth/components/auth-layout/auth-layout.component';
 import { CreatePostComponent } from './modules/posts/pages/create-post/create-post.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'posts', pathMatch: 'full'},
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+        ],
+    },
     { path: 'posts', component: HomeComponent },
     { path: 'posts/create', component: CreatePostComponent },
-    { path: 'auth/login', component: LoginComponent },
-    { path: 'auth/register', component: RegisterComponent },
+    { path: '', redirectTo: 'posts', pathMatch: 'full' },
 ];
